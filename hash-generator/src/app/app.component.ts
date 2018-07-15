2a4117641a992ec49147fcf21e4f501976402c14
davulurikoti <davulurikoti@gmail.com>
2018-07-14 20:38:57 -0500

    Hash Generator Project

---
+++ hash-generator/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import * as crypto from 'crypto-js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  text = '';
+  output :string=crypto.HmacSHA256('', crypto.SHA256("key")).toString();;
+  onSearchChange(searchValue : string ) {  
+    this.output = crypto.HmacSHA256(searchValue, crypto.SHA256("key")).toString();
+  }
+}
